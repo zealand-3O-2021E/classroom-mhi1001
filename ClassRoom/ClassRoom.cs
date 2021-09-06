22931172d2925e56b67f1d032f96be5344772341
pc <pc@DESKTOP-BI5FP4B>
2021-09-06 18:18:05 +0200

    Added classroom and its properties

---
+++ ClassRoom/ClassRoom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class ClassRoom
+    {
+
+        public string ClassName { get; set; }
+
+        public List<Student> ClassList { get; set; }
+
+        public DateTime SemesterStart { get; set; }
+
+    }
+}
